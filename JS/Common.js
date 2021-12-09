9a337efa55894631032045f1c018c5dc0c17890e
MikeMyers59 <95873583+MikeMyers59@users.noreply.github.com>
2021-12-09 09:46:19 -0800

    Update Common.js

--- JS/Common.js
+++ JS/Common.js
@@ -2,13 +2,9 @@
 var ScriptName = "Common.js";
 var ScriptVersion = "1.0";
 
-// Subroutines for Windows Apps
-Sub OpenNotepad()
-  CreateObject("WScript.Shell").Run "notepad"
-End Sub
+// Routines for Windows Apps
 
-// Subroutines for Windows OS
-Sub Shutdown()
-  CreateObject("WScript.Shell").Run "cmd /c shutdown /s /t 30"
-End Sub
+
+// Routines for Windows OS
+
 
