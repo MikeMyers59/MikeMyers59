0eeb5e99b9037583091467e70943bf422c1d8374
MikeMyers59 <95873583+MikeMyers59@users.noreply.github.com>
2021-12-09 09:54:34 -0800

    Update Common.js

--- JS/Common.js
+++ JS/Common.js
@@ -3,8 +3,15 @@ var ScriptName = "Common.js";
 var ScriptVersion = "1.0";
 
 // Routines for Windows Apps
-
+function Launch(program){
+  (new ActiveXObject('WScript.Shell')).Run(program);void(0);
+}
 
 // Routines for Windows OS
+function popWindow(url, name){
+  var wndPopup = window.open(url,name,"");
+  // Bring new window to foreground
+  if(wndPopup) wndPopup.focus();
+}
 
 
