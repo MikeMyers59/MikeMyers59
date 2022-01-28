0ebf92f69b0a0e91ce3d278acd54aeecdfc64eb8
MikeMyers59 <95873583+MikeMyers59@users.noreply.github.com>
2022-01-28 08:39:00 -0800

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 100, 
+  tabWidth: 2, 
+  useTabs: false, 
+  semi: false, 
+  singleQuote: true, 
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true, 
+  trailingComma: 'es5', 
+  bracketSpacing: true, 
+  jsxBracketSameLine: false, 
+  arrowParens: 'avoid', 
+  proseWrap: 'preserve', // markdown wrap
+  htmlWhitespaceSensitivity: 'css'
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+}
